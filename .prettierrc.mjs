560d5c9b4270af00acb1456a31ee88c4f39a7010
laurentDellaNegra <laurent.dellanegra@gmail.com>
2024-05-20 22:26:30 +0200

    ✨ Add the grid

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -1,5 +1,9 @@
 /** @type {import("prettier").Config} */
 export default {
+  trailingComma: 'es5',
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
   plugins: ['prettier-plugin-astro'],
   overrides: [
     {
